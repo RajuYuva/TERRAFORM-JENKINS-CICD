d3adff0e96da629bbc52f3af9bb7eb59bf0477fe
RajuYuva <112694998+RajuYuva@users.noreply.github.com>
2023-12-21 21:51:17 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,7 @@
 terraform {
   backend "s3" {
-    bucket         = "mrcloudbook-cicd-bucket"
+    bucket         = "naga-test-bucket-jenkins"
     key            = "my-terraform-environment/main"
-    region         = "ap-south-1"
-    dynamodb_table = "mrcloudbook-dynamo-db-table"
+    region         = "eu-west-1"
   }
 }
